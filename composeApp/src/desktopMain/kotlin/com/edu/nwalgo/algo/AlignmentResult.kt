f440f9cac33a2c025ada5218f61750d57395ede0
timGalk <tivron@outlook.com>
2025-04-12 15:58:18 +0200

    Add docstrings to Algorithm method

--- composeApp/src/desktopMain/kotlin/com/edu/nwalgo/algo/AlignmentResult.kt
+++ composeApp/src/desktopMain/kotlin/com/edu/nwalgo/algo/AlignmentResult.kt
@@ -1,5 +1,17 @@
 package com.edu.nwalgo.algo
 
+
+/**
+ * Represents the result of a sequence alignment operation.
+ *
+ * @property alignedSeq1 The first aligned sequence as a string.
+ * @property alignedSeq2 The second aligned sequence as a string.
+ * @property scoreMatrix A 2D array representing the scoring matrix used in the alignment.
+ * @property path A list of coordinate pairs representing the alignment path, useful for GUI grid path drawing.
+ * @property identityPercent The percentage of identical matches between the two sequences.
+ * @property gapCount The total number of gaps introduced in the alignment.
+ * @property score The final alignment score.
+ */
 data class AlignmentResult(
     val alignedSeq1: String,
     val alignedSeq2: String,
